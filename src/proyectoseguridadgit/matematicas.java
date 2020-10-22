9082b71bc877015adcbb4e6254dd93e57ee65767
cristianleopoldo <cristianramirez1989@hotmail.com>
2020-10-22 11:49:21 -0500

    primer paso

---
+++ src/proyectoseguridadgit/matematicas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoseguridadgit;
+
+/**
+ *
+ * @author cristian_leopooldo
+ */
+public class matematicas {
+    
+    public int sumar(int x, int y){
+        return x+y;
+    }
+    
+}
